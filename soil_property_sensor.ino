78f5c00fef312269cecb31f2e64a6a32e1a468b1
henschec <33008794+henschec@users.noreply.github.com>
2017-12-17 16:13:00 -0600

    Update soil_property_sensor.ino

--- soil_property_sensor.ino
+++ soil_property_sensor.ino
@@ -56,7 +56,7 @@ void loop() {
   Serial.print("Moisture Content (Vol water/ Vol Soil) = ");
   if(sensor_analog > 220)
   {
-    volwaterovervolsoil = (sensor_analog - 1023)/(-2580.8);
+    volwaterovervolsoil = (sensor_analog - 1023)/(-2580.8); //formula comes from calibration, needs to be recalibrated for different soil samples
     Serial.print(volwaterovervolsoil);
   }
   else
@@ -64,6 +64,7 @@ void loop() {
     Serial.print("Greater than 0.3111");
   }
   Serial.print("\n\n");
+  
   // pH:
   float phvalue = 3.5*(float)analogRead(pH_pin)*5.0/1024; //the 5.0/1024 converts to volts, the 3.5 converts to pH value
   Serial.print("    pH:");
